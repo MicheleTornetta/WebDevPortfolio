582615152b8c032fa9fdf28c55f8ea1e39739c43
MicheleTornetta <112029252+MicheleTornetta@users.noreply.github.com>
2023-02-04 17:44:14 -0500

    start on portfolio page

--- portfolio/src/components/ProjectComponent.js
+++ portfolio/src/components/ProjectComponent.js
@@ -1,10 +1,25 @@
-import React from 'react';
-// Here we are importing a CSS file 
+import React from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
-function Project(props) {
+function Project({ title, image, deployedLink, github }) {
   return (
     <div>
-      <h1>Project: {props.title}</h1>
+        <Card style={{ width: "18rem" }}>
+          <Card.Body>
+            <Card.Title>{title}</Card.Title>
+            <Card.Img variant="top" src={image} />
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button href={deployedLink} target="_blank" variant="primary">See Project</Button>
+            <Button href={github} target="_blank" variant="secondary">See Github</Button>
+          </Card.Body>
+        </Card>
     </div>
   );
 }
--- portfolio/src/components/pages/Portfolio.js
+++ portfolio/src/components/pages/Portfolio.js
@@ -1,18 +1,69 @@
-import React from 'react';
-import Project from '../ProjectComponent';
+import React from "react";
+import Project from "../ProjectComponent";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 export default function Portfolio() {
   return (
     <div>
       <h1>Portfolio</h1>
-      
-      <h2>My projects</h2>
 
-      <Project 
-        title="WebDevPortolio" 
-        image="iamge.png" 
-        deployedLink="https://www.google.com/" 
-        github="https://www.github.com/MicheleTornetta/" />
+      <h2>My projects</h2>
+      <Container>
+        <Row>
+          <Col xs={12} md={6} lg={4}>
+            <Project
+              title="WebDevPortolio"
+              image="image.png"
+              deployedLink="https://www.google.com/"
+              github="https://www.github.com/MicheleTornetta/"
+            />
+          </Col>
+          <Col xs={12} md={6} lg={4}>
+            <Project
+              title="WebDevPortolio"
+              image="image.png"
+              deployedLink="https://www.google.com/"
+              github="https://www.github.com/MicheleTornetta/"
+            />
+          </Col>
+          <Col xs={12} md={6} lg={4}>
+            <Project
+              title="WebDevPortolio"
+              image="image.png"
+              deployedLink="https://www.google.com/"
+              github="https://www.github.com/MicheleTornetta/"
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={4}>
+            <Project
+              title="WebDevPortolio"
+              image="iamge.png"
+              deployedLink="https://www.google.com/"
+              github="https://www.github.com/MicheleTornetta/"
+            />
+          </Col>
+          <Col sm={8}>
+            <Project
+              title="WebDevPortolio"
+              image="iamge.png"
+              deployedLink="https://www.google.com/"
+              github="https://www.github.com/MicheleTornetta/"
+            />
+          </Col>
+          <Col sm={8}>
+            <Project
+              title="WebDevPortolio"
+              image="iamge.png"
+              deployedLink="https://www.google.com/"
+              github="https://www.github.com/MicheleTornetta/"
+            />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
